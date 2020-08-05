7a40551b04af244eea9629dc58d204d05302e417
Dinh Phu <ndp17hp@gmail.com>
2020-08-05 08:38:18 +0700

    set up thanh cong

---
+++ src/test/java/country/com/codegym/ApplicationTests.java
@@ -0,0 +1,13 @@
+package country.com.codegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
